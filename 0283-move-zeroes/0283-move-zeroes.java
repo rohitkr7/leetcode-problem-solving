f9171b667f52a2a09c03c757dca25bfa1b9dcbf7
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-22 17:00:38 +0530

    Time: 2 ms (91.97%), Space: 48 MB (6.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // "relative order" does not mean sorting by value—it means preserving the original sequence of appearance from left to right.
-        
+
+        // Time: O(n) and Space: O(1)
         int insertPos = 0;
 
         for (int i = 0; i < nums.length; i++) {
