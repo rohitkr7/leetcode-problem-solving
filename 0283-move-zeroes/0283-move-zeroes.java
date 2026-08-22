c0a8f0314c14acb8d67d32510b1144ffcb70cced
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-22 16:59:29 +0530

    Time: 2 ms (91.97%), Space: 47.4 MB (96.19%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // "relative order" does not mean sorting by value—it means preserving the original sequence of appearance from left to right.
+        
+        int insertPos = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap non-zero element with the element at insertPos
+                int temp = nums[insertPos];
+                nums[insertPos] = nums[i];
+                nums[i] = temp;
+
+                insertPos++;
+            }
+        }
+    }
+}
