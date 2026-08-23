d848e9feaeab020f9e864e2a15e0460008444fdd
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-23 22:21:16 +0530

    Time: 1 ms (96.91%), Space: 47.2 MB (80.3%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,27 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode prev = null;
+class Solution {
+
+    // Time: O(n) and Space:  O(1)
+    public ListNode removeElements_1(ListNode head, int val) {
+
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode prev = new ListNode(0, head);
         ListNode curr = head;
-        
-        while(curr != null){
-            if(curr.val == val){
-                if(curr == head){
-                    curr = curr.next;
-                    head = curr;
-                }
-                else{
-                    prev.next = curr.next;
-                    curr = curr.next;
-                }
-            }
-            else{
+        while (curr != null) {
+            if (curr.val == val)
+                prev.next = curr.next;
+            else
                 prev = curr;
+
+            curr = curr.next;
+        }
+
+        return head;
+    }
+
+    // Clean Solution
+    public ListNode removeElements(ListNode head, int val) {
+        // Create a dummy node pointing to the original head
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = dummy;
+        
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                // Skip the target node
+                curr.next = curr.next.next;
+            } else {
+                // Only advance if the next node is valid
                 curr = curr.next;
             }
         }
-        return head;
+        
+        return dummy.next;
     }
 }
