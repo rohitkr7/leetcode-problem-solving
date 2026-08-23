5428d3110b81b99fea2073ecd7a10c6aea18173e
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-23 09:15:29 +0530

    Fix indentation and formatting in containsDuplicate method

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,15 +3,14 @@ class Solution {
 
         // Time: O(n)
         // Space: O(n)
+        
         HashSet hs = new HashSet<Integer>();
-
         for(int i : nums){
             if(hs.contains(i))
                 return true;
             else
-            hs.add(i);
+                hs.add(i);
         }
-
         return false;
     }
 }
