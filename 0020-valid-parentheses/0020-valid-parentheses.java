344705fe101cce6066770bc423e23d56305c8190
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-25 20:54:44 +0530

    Time: 3 ms (86.11%), Space: 43.4 MB (25.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,62 @@
 import java.util.Stack;
+
 class Solution {
-    
-    
-    
+
+    // Time: O(n) and Space: O(n) space stack
     public boolean isValid(String s) {
-        
+
+        // Fast exit: odd length can never be balanced
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
         HashMap<Character, Character> bracketPair = new HashMap<Character, Character>();
-        bracketPair.put(')','(');
-        bracketPair.put('}','{');
-        bracketPair.put(']','[');
-        
+        bracketPair.put(')', '(');
+        bracketPair.put('}', '{');
+        bracketPair.put(']', '[');
+
         Stack<Character> st = new Stack<Character>();
-        char top ;
-        for(char c : s.toCharArray()){
-            if(c=='(' || c=='{' || c=='[' ){
+        char top;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
-            }else{
-                if(!st.isEmpty()){
+            } else {
+                if (!st.isEmpty()) {
                     top = st.peek();
-                    if(top == bracketPair.get(c)){
+                    if (top == bracketPair.get(c)) {
                         st.pop();
-                    }else{
-                       return false; 
+                    } else {
+                        return false;
                     }
-                }else{
+                } else {
                     return false;
                 }
             }
         }
-        
+
         return st.size() == 0;
+    }
+
+    public boolean isValid_simple(String s) {
+        // Fast exit: odd length can never be balanced
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
