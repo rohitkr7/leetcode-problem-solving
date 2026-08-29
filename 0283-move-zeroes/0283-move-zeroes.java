286670f080d86067279641941ccd0eb720fdb493
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-08-29 21:52:05 +0530

    Time: 3 ms (17.95%), Space: 48 MB (6.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -12,11 +12,11 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // Swap non-zero element with the element at insertPos
-                int temp = nums[insertPos];
-                nums[insertPos] = nums[i];
-                nums[i] = temp;
-
+                if (i != insertPos) {
+                    int temp = nums[insertPos];
+                    nums[insertPos] = nums[i];
+                    nums[i] = temp;
+                }
                 insertPos++;
             }
         }
