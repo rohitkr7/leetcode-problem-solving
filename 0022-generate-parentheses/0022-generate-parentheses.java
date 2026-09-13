532b2840363b9bd61ae83ac2d821fb050185ffec
Rohit Roy <26903384+rohitkr7@users.noreply.github.com>
2026-09-13 17:02:36 +0530

    Time: 1 ms (85.37%), Space: 44.5 MB (72.42%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,23 +1,28 @@
 class Solution {
-    
+    // Time: O(4^n / sqrt{n})
+    // Space: O(n)
     public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        backtrack(list, "", 0, 0, n);
-        return list;
+        List<String> result = new ArrayList<>();
+        dfs(0, 0, n, new StringBuilder(), result);
+        return result;
     }
-    
-    public void backtrack(List<String> list, String str, int open, int close, int max){
-        System.out.println(str);
-        if(str.length() == max*2){
-            list.add(str);
+
+    private void dfs(int open, int close, int length, StringBuilder currentResult, List<String> result) {
+        if (currentResult.length() == 2 * length) {
+            result.add(currentResult.toString());
             return;
         }
-        
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
-        
-        if(close < open)
-            backtrack(list, str+")", open, close+1, max);
-       
+
+        if (open < length) {
+            currentResult.append("(");
+            dfs(open + 1, close, length, currentResult, result);
+            currentResult.deleteCharAt(currentResult.length() - 1);
+        }
+
+        if (close < open) {
+            currentResult.append(")");
+            dfs(open, close + 1, length, currentResult, result);
+            currentResult.deleteCharAt(currentResult.length() - 1);
+        }
     }
 }
